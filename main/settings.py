37904bbfba657beb832ce5b4a1628a35e2b78feb
eshat002 <eshatjubayer22@outlook.com>
2023-12-09 00:24:16 +0600

    dss

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8z-xiqf3u&k#sr5c9060k%ks97dg!*$&)2!hskg=j&w8nsoy90
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://web-production-4d22.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 CSRF_TRUSTED_ORIGINS = ["https://web-production-4d22.up.railway.app/"]
